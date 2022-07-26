7d17007c198a07bd5af109115d52a6bc3b1bdc7c
seohyunz <joseohyun0201@gmail.com>
2022-07-26 11:15:12 +0900

    에러,예외처리

--- app05.js
+++ app05.js
@@ -8,3 +8,23 @@ setInterval(()=>{
         console.log(err);
     }
 },500);
+
+const fs = require('fs'); //파일 삭제
+setInterval(()=>{
+    fs.unlink('./sam.txt',(err)=>{
+        if(err){
+            console.log(err);
+        }
+    });
+},500);
+
+//예측 불가능한 에러처리
+process.on('uncaughtException',(err)=>{
+    console.error('예기치 못한 에러',err);
+});
+setInterval(()=>{
+    throw new Error('무한출력');
+},1000);
+setTimeout(()=>{
+    console.log('실행됩니다.');
+},2000);
