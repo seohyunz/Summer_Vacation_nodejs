5b6b5f3a911ebe63399f84d157ff94c69c9997fa
seohyunz <joseohyun0201@gmail.com>
2022-07-26 14:24:03 +0900

    이미지,쿠키

---
+++ server02.js
@@ -0,0 +1,27 @@
+var fs = require('fs');
+var http = rquire('http');
+http.createServer(function(request, response){
+   //이미지파일 읽기
+    fs.readFile('./nodejs/eximage.png', function(error,data){
+        response.writeHead(200,{'Content-Type':'image/jpeg'});
+        response.end(data);
+    });
+}).listen(4444,function(){
+    console.log('4444번 포트에서 서버 대기 중입니다.');
+});
+
+//쿠키 생성하기
+
+//모듈 추출
+var http = require('http');
+http.createServer(function(reuest,response){
+    //쿠키 생성
+    response.writeHead(200,{
+        'Content-Type':'text/html',
+        'Set-Cookie':['breakfast = toast', 'dinner = chicken']
+    });
+//쿠키 출력
+    response.end('<h1>'+ http.request.headers.cookie+'<h1>');
+}).listen(4444, function(){
+    console.log('서버 대기 중');
+})
