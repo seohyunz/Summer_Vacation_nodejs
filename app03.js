f92fe952456e1f2839988600c0e84dfb512f4045
seohyunz <joseohyun0201@gmail.com>
2022-07-26 10:27:31 +0900

    fs모듈

--- app03.js
+++ app03.js
@@ -1,4 +1,5 @@
 //fs모듈
+const { rejects } = require('assert');
 const fs = require('fs');
 fs.readFile('./sam.txt', (err,data)=>{
     if(err){
@@ -52,3 +53,47 @@ fs.readFileSync('./sam.txt',(err,data)=>{
     console.log('3',data.toString());
 });
 console.log('끝');
+
+
+//비동기식 순서대로 결과가 나오도록 하기
+//readFile의 콜백 다음에 readFile추가
+const fs = require('fs'); 
+const { resolve } = require('path');
+console.log('시작');
+fs.readFile('./sam.txt',(err,data)=>{
+    if(err){
+        throw err;
+    }
+    console.log('1',data.toString());
+    fs.readFile('./sam.txt',(err,data)=>{
+        if(err){
+            throw err;
+    }
+    console.log('2',data.toString());
+    fs.readFile('./sam.txt',(err,data)=>{
+        if(err){
+            throw err;
+    }
+    console.log('3',data.toString());
+    console.log('끝');
+        });
+    });
+});
+
+//프로미스
+const condition = true;
+const promise = new Promise((resolve,rejects)=>{
+    if(condition){
+        resolve('성공');
+    }
+    else{
+        rejects('실패');
+    }
+});
+promise.then((message)=>{
+    console.log(message);
+}).catch((err)=>{
+    console.error(err);
+}).finally(()=>{
+    console.log('무조건')
+});
